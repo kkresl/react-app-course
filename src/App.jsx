24d321d7ff8356a8f67ddbf8e90fef1ee018d2c3
Maryia Samtsevich <marie.skat6@gmail.com>
2026-08-26 18:46:12 +0200

    not found page, question card props, dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter } from "react-router-dom";
 import { MainLayout } from "./components/MainLayout/MainLayout";
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
-  //return <MainLayout />;
   return (
     <BrowserRouter>
       <Routes>
@@ -12,6 +12,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/forbidden" element={<div> forbidden </div>} />
           <Route path="/addquestion" element={<div> addquestion </div>} />
+          <Route path="/question/:id" element={<div> question page </div>} />
+          {/* добавляет динамику ":" */}
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,20 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import { Button } from "../Button";
 import cls from "./QuestionCard.module.css";
 
-export const QuestionCard = () => {
+export const QuestionCard = ({ card }) => {
+const navigate = useNavigate();
+
   return (
     <div className={cls.card}>
       {" "}
       <div className={cls.cardLabels}>
-        <div> Level: 1</div>
-        <div> Not Complited </div>
+        <div> Level: {card.level} </div>
+        <div> {card.completed ? "Completed" : "Not completed"}</div>
       </div>
-      <h5 className={cls.cardTitle}> Что такое jsx </h5>
+      <h5 className={cls.cardTitle}> {card.question} </h5>
       <div className={cls.cardAnswers}>
         <label> short answer </label>
-        <p className={cls.cardAnswer}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, unde. </p>
+        <p className={cls.cardAnswer}> {card.answer} </p>
       </div>
-      <Button onClick={() => {}}> View </Button>
+      <Button onClick={() => navigate(`/question/${card.id}`)}> View </Button>
     </div>
   );
 };
