41d5555242928a905a7952c1d3cdad059d2acb04
Maryia Samtsevich <marie.skat6@gmail.com>
2026-09-03 11:15:48 +0200

    Badge component

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,16 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../Button";
 import cls from "./QuestionCard.module.css";
+import { Badge } from "../Badge";
 
 export const QuestionCard = ({ card }) => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
+
+  const levelVariant = card.level === 1 ? "primary" : card.level === 2 ? "warning" : "alert";
+  const complitedVariant = card.completed ? "success" : "primary";
 
   return (
     <div className={cls.card}>
-      {" "}
       <div className={cls.cardLabels}>
-        <div> Level: {card.level} </div>
-        <div> {card.completed ? "Completed" : "Not completed"}</div>
+        <Badge variant={levelVariant}> Level: {card.level} </Badge>
+        <Badge variant={complitedVariant}> {card.completed ? "Completed" : "Not completed"}</Badge>
       </div>
       <h5 className={cls.cardTitle}> {card.question} </h5>
       <div className={cls.cardAnswers}>
