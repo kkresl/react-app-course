8fbe56a6b2e23adcbc5de0245e3544477f456291
Maryia Samtsevich <marie.skat6@gmail.com>
2026-09-03 12:07:51 +0200

    sort logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 import { API_URL } from "../../constants/index";
 import { useFetch } from "../../hooks/useFetch";
 import { QuestionCardList } from "../../components/QuestionCardList";
@@ -10,9 +10,10 @@ import { SearchInput } from "../../components/SearchInput";
 export const HomePage = () => {
   const [questions, setQuestions] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [sortSelectValue, setSortSelectValue] = useState("");
 
   const [getQuestions, isLoading, error] = useFetch(async (url) => {
-    const response = await fetch(`${API_URL}/${url}`);
+    const response = await fetch(`${API_URL}/${url}?_sort=completed`);
     const questions = await response.json();
 
     setQuestions(questions);
@@ -24,28 +25,51 @@ export const HomePage = () => {
     /* описываем состояние ; */
   }
 
+  const cards = useMemo(() => {
+    return questions.filter((d) => d.question.toLowerCase().includes(searchValue.trim().toLowerCase()));
+  }, [questions, searchValue]);
+
+  {
+    /* логика поиска. Приводим к нижнему, убираем пробелы и тд. Оборачиваем в useMemo, чтобы не было лишних перерисовок */
+  }
+
   useEffect(() => {
-    getQuestions("react");
-  }, []);
+    getQuestions(`react?${sortSelectValue}`);
+  }, [sortSelectValue]);
 
   {
     /* {} - что делать, []- когда повторять (dependencies) */
   }
 
   const onSearchChangeHandler = (e) => {
-    console.log(e.target.value);
     setSearchValue(e.target.value);
+  };
+
+  const onSortSelectChangeHandler = (e) => {
+    setSortSelectValue(e.target.value);
   };
 
   return (
     <>
       <div className={cls.controlsContainer}>
         <SearchInput value={searchValue} onChange={onSearchChangeHandler} />
+
+        <select value={sortSelectValue} onChange={onSortSelectChangeHandler} className={cls.select}>
+          <option value="">sort by</option>
+          <hr />
+          <option value="_sort=level"> level ASC</option>
+          <option value="_sort=-level"> level DESC</option>
+          <option value="_sort=completed"> complited ASC</option>
+          <option value="_sort=-completed"> complited DESC</option>
+
+        </select>
       </div>
 
       {isLoading && <Loader />}
       {error && <p> {error} </p>}
-      <QuestionCardList cards={questions} />
+      {cards.length === 0 && <p className={cls.noCardsInfo}> No cards... </p>}
+
+      <QuestionCardList cards={cards} />
     </>
   );
 };
