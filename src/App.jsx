2186755b3e00569514e262f2526c22bc214c9f83
Maryia Samtsevich <marie.skat6@gmail.com>
2026-09-06 14:08:44 +0200

    adding new question

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,28 @@ import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { QuestionPage } from "./pages/QuestionPage";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { AddQuestionPage } from "./pages/AddQuestionPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/forbidden" element={<div> forbidden </div>} />
-          <Route path="/addquestion" element={<AddQuestionPage />} />
-          <Route path="/question/:id" element={<QuestionPage />} />
-          {/* добавляет динамику ":" */}
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/forbidden" element={<div> forbidden </div>} />
+            <Route path="/addquestion" element={<AddQuestionPage />} />
+            <Route path="/question/:id" element={<QuestionPage />} />
+            {/* добавляет динамику ":" */}
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      <ToastContainer />
+    </>
   );
 }
 
--- src/pages/AddQuestionPage/AddQuestionPage.jsx
+++ src/pages/AddQuestionPage/AddQuestionPage.jsx
@@ -1,17 +1,55 @@
+import { useActionState } from "react";
 import { Button } from "../../components/Button";
 import cls from "./AddQuestionPage.module.css";
+import { delayFn } from "../../helpers/delayFn";
+import { toast } from "react-toastify";
+import { API_URL } from "../../constants";
+
+const createCardAction = async (_prevState, formData) => {
+  try {
+    await delayFn();
+
+    const newQuestion = Object.fromEntries(formData);
+    const resources = newQuestion.resources.trim();
+    const isClearForm = newQuestion.clearForm; //formData.get("clearForm")
+
+    const response = await fetch(`${API_URL}/react`, {
+      method: "POST",
+      body: JSON.stringify({
+        question: newQuestion.question,
+        answer: newQuestion.answer,
+        description: newQuestion.description,
+        resources: resources.length ? resources.split(",") : [],
+        level: Number(newQuestion.level),
+        completed: false,
+        editDate: undefined,
+      }),
+    });
+
+    const question = response.json();
+    toast.success("New question is successfully created", {
+      autoClose: 1200,
+    });
+
+    return isClearForm ? {} : question;
+  } catch (error) {
+    toast.error(error.message);
+  }
+};
 
 export const AddQuestionPage = () => {
+  const [formState, formAction, isPending] = useActionState(createCardAction, { clearForm: true });
+
   return (
     <>
       <h1 className={cls.formTitle}> Add new question </h1>
 
       <div className={cls.formContainer}>
-        <form action="" className={cls.form}>
+        <form action={formAction} className={cls.form}>
           <div className={cls.formControl}>
             <label htmlFor="questionField"> Question: </label>
             <textarea
-              defaultValue={"defaultValue"}
+              defaultValue={formState.question}
               name="question"
               id="questionField"
               cols="30"
@@ -24,7 +62,7 @@ export const AddQuestionPage = () => {
           <div className={cls.formControl}>
             <label htmlFor="answerField"> Short Answer: </label>
             <textarea
-              defaultValue={"defaultValue"}
+              defaultValue={formState.answer}
               name="answer"
               id="answerField"
               cols="30"
@@ -37,7 +75,7 @@ export const AddQuestionPage = () => {
           <div className={cls.formControl}>
             <label htmlFor="descriptionField"> Description: </label>
             <textarea
-              defaultValue={"defaultValue"}
+              defaultValue={formState.description}
               name="description"
               id="descriptionField"
               cols="30"
@@ -50,7 +88,7 @@ export const AddQuestionPage = () => {
           <div className={cls.formControl}>
             <label htmlFor="resourcesField"> Resources: </label>
             <textarea
-              defaultValue={"defaultValue"}
+              defaultValue={formState.resources}
               name="resources"
               id="resourcesField"
               cols="30"
@@ -62,7 +100,7 @@ export const AddQuestionPage = () => {
 
           <div className={cls.formControl}>
             <label htmlFor="LevelField"> Level: </label>
-            <select name="level" id="levelField" defaultValue={"defaultValue"}>
+            <select name="level" id="levelField" defaultValue={formState.level}>
               <option disabled> Question level </option>
               <hr />
               <option value="1"> 1 - easiest </option>
@@ -72,11 +110,17 @@ export const AddQuestionPage = () => {
           </div>
 
           <label htmlFor="clearFormField" className={cls.clearFormControl}>
-            <input className={cls.checkbox} type="checkbox" name="clearForm" id="clearFormField" defaultValue={true} />
+            <input
+              className={cls.checkbox}
+              type="checkbox"
+              name="clearForm"
+              id="clearFormField"
+              defaultChecked={formState.clearForm}
+            />
             <span> clear form after submitting? </span>
           </label>
 
-          <Button>Add question </Button>
+          <Button isDisabled={isPending}>Add question </Button>
         </form>
       </div>
     </>
